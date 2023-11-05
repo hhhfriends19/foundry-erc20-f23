a8fa6a1ab5bc0da251893e23cb3f70bbfa1f6b46
hhhfriends19 <84479916@qq.com>
2023-11-05 18:02:08 +0800

    add function totalSupply, decimals

--- src/ManualToken.sol
+++ src/ManualToken.sol
@@ -6,4 +6,12 @@ contract ManualToken {
     function name() public pure returns (string memory) {
         return "Manual Token";
     }
+
+    function totalSupply() public pure returns (uint256) {
+        return 100 ether; // 100000000000000000000
+    }
+
+    function decimals() public pure returns (uint8) {
+        return 18;
+    }
 }
