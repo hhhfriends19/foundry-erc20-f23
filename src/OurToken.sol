e7312425ee056c5460bdd447d01f4c3369263d10
hhhfriends19 <84479916@qq.com>
2023-11-06 20:36:38 +0800

    finish OurToken.sol and modify foundry.toml

--- src/OurToken.sol
+++ src/OurToken.sol
@@ -2,4 +2,10 @@
 
 pragma solidity ^0.8.18;
 
-contract OurToken {}
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract OurToken is ERC20 {
+    constructor(uint initialSupply) ERC20("OurToken", "OT") {
+        _mint(msg.sender, initialSupply);
+    }
+}
