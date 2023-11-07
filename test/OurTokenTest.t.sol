28dce75b1953126b30ed90f37b0b09f33b9bf41b
hhhfriends19 <84479916@qq.com>
2023-11-07 20:00:46 +0800

    modify OurTokenTest.t.sol

--- test/OurTokenTest.t.sol
+++ test/OurTokenTest.t.sol
@@ -42,4 +42,31 @@ contract OurTokenTest is Test {
         assertEq(ourToken.balanceOf(alice), transferAmount);
         assertEq(ourToken.balanceOf(bob), STARTING_BALANCE - transferAmount);
     }
+
+    function testTransfer() public {
+        uint256 amount = 1000;
+        address receiver = address(0x1);
+        vm.prank(msg.sender);
+        ourToken.transfer(receiver, amount);
+        assertEq(ourToken.balanceOf(receiver), amount);
+    }
+
+    function testBalanceAfterTransfer() public {
+        uint256 amount = 1000;
+        address reveiver = address(0x1);
+        uint256 initialBalance = ourToken.balanceOf(msg.sender);
+
+        vm.prank(msg.sender);
+        ourToken.transfer(reveiver, amount);
+        assertEq(ourToken.balanceOf(msg.sender), initialBalance - amount);
+    }
+
+    function testTransferFrom() public {
+        uint256 amount = 1000;
+        address receiver = address(0x1);
+        vm.prank(msg.sender);
+        ourToken.approve(address(this), amount);
+        ourToken.transferFrom(msg.sender, receiver, amount);
+        assertEq(ourToken.balanceOf(receiver), amount);
+    }
 }
